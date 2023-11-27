426ac8fd6dba799a69e69cae48da5f5054e6e270
Liam Warren <lbreenwarren@gmail.com>
2023-11-27 14:20:29 -0500

    cs416 lab20

---
+++ cs416_2.0/lab20/src/SelectionSort.java
@@ -0,0 +1,18 @@
+/**A program to sort a list of samples using Selection Sort.
+ * @author Liam Warren
+ * @version 11.27.23
+ */
+public class SelectionSort extends Sort {
+
+    /**Constructor for SelectionSort.
+     * @param filename data file
+     */
+    public SelectionSort(String filename) {
+        super(filename);
+    }
+    
+    /**sort method not yet implemented. */
+    public void sort() {
+        
+    }
+}
